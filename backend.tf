fdf036752c9b9106932adf155561e1197b0643d2
VIVESH <35293085+574n13y@users.noreply.github.com>
2023-11-22 22:54:00 +0530

    commit v1

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-   organization = "Iac_vivesh"
+   organization = "Iac_Stanley_NetOps"
     workspaces {
       name = "ace-iac-day-zero"
     }
